a6ebddbccd3e5115093fc50a05281f685149f905
Daniel Flores Hurtado <daniel.hurtado4774@gmail.com>
2023-07-06 15:04:56 -0500

    Functional chart from CSV file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'inport-CSV-File';
+
+  allTitles: string[] = [];
+  csvdata: any[] = [];
+
+  async loadFile(inputEvent: any) {
+
+    const file: File = inputEvent.target.files[0];
+
+    let fileContent = await file.text();
+
+    fileContent.split("\n").forEach((ele, index) => {
+      if (index === 0) {
+        ele.split(",").forEach((title) => {
+          this.allTitles.push(title);
+        })
+      } else { 
+        let user: any[] = [];
+        ele.split(",").forEach((ele) => {
+            user.push(ele);
+        }
+      )
+      this.csvdata.push(user);
+    }
+    })
+
+  }
 }
